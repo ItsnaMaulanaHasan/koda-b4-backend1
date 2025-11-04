608fc9f4c6e2c049365f5713cbd5fb315b7e56db
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-05 01:36:04 +0700

    feat: create models for user dan response

---
+++ models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	Id       int    `json:"id" form:"id" xml:"id"`
+	Username string `json:"username" form:"username" xml:"username" binding:"required,min=3,max=20"`
+	Email    string `json:"email" form:"email" xml:"email" binding:"required,email"`
+	Password string `json:"password" form:"password" xml:"password" binding:"required,min=6"`
+}
