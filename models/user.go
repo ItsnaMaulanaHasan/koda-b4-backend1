70da8831a074c631f60447996bb5c8dc10720cfa
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-05 02:32:12 +0700

    feat: adjust password validation

--- models/user.go
+++ models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	Id       int    `json:"id" form:"id" xml:"id"`
 	Username string `json:"username" form:"username" xml:"username" binding:"required,min=3,max=20"`
 	Email    string `json:"email" form:"email" xml:"email" binding:"required,email"`
-	Password string `json:"password" form:"password" xml:"password" binding:"required,min=6"`
+	Password string `json:"password,omitempty" form:"password" xml:"password" binding:"required,min=6"`
 }
