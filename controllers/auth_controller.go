e5d9dea327c1798dac1f494b538ec8f58d842494
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-05 13:35:11 +0700

    feat: create method forgot password and change shouldbind to shouldbindbodywitjson

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -20,7 +20,7 @@ func NewAuthController(uc *UserController) *AuthController {
 
 func (ac *AuthController) Register(ctx *gin.Context) {
 	var body models.User
-	err := ctx.ShouldBind(&body)
+	err := ctx.ShouldBindBodyWithJSON(&body)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, models.Response{
 			Success: false,
@@ -64,17 +64,10 @@ func (ac *AuthController) Register(ctx *gin.Context) {
 	body.Password = string(hashPassword)
 	ac.userController.users = append(ac.userController.users, body)
 
-	responseData := models.User{
-		Id:       body.Id,
-		Username: body.Username,
-		Email:    body.Email,
-		Password: body.Password,
-	}
-
 	ctx.JSON(http.StatusCreated, models.Response{
 		Success: true,
 		Message: "User registered successfully",
-		Data:    responseData,
+		Data:    body,
 	})
 }
 
@@ -84,7 +77,7 @@ func (ac *AuthController) Login(ctx *gin.Context) {
 		Password string `json:"password" form:"password" xml:"password" binding:"required,min=6"`
 	}
 
-	err := ctx.ShouldBind(&loginData)
+	err := ctx.ShouldBindBodyWithJSON(&loginData)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, models.Response{
 			Success: false,
@@ -135,4 +128,44 @@ func (ac *AuthController) Login(ctx *gin.Context) {
 		Message: "User login Successfully",
 		Data:    responseData,
 	})
+}
+
+func (ac *AuthController) ForgotPassword(ctx *gin.Context) {
+	email := ctx.Param("email")
+	var newPassword string
+
+	err := ctx.ShouldBindBodyWithJSON(&newPassword)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: true,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	argon := argon2.DefaultConfig()
+	hashPassword, err := argon.HashEncoded([]byte(newPassword))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "Hash password failed",
+		})
+		return
+	}
+
+	var foundUser *models.User
+	for i := range ac.userController.users {
+		if ac.userController.users[i].Email == email {
+			ac.userController.users[i].Password = string(hashPassword)
+			foundUser = &ac.userController.users[i]
+		}
+	}
+
+	if foundUser != nil {
+		ctx.JSON(http.StatusOK, models.Response{
+			Success: true,
+			Message: "Password success updates",
+			Data:    foundUser,
+		})
+	}
 }
