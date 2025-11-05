55eda43e61377d22370a6682f4f792825c35562f
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-05 14:42:41 +0700

    feat: adjust tag struct on user and response struct

--- models/user.go
+++ models/user.go
@@ -1,8 +1,8 @@
 package models
 
 type User struct {
-	Id       int    `json:"id" form:"id" xml:"id"`
-	Username string `json:"username" form:"username" xml:"username" binding:"required,min=3,max=20"`
-	Email    string `json:"email" form:"email" xml:"email" binding:"required,email"`
-	Password string `json:"password,omitempty" form:"password" xml:"password" binding:"required,min=6"`
+	Id       int    `json:"id"`
+	Username string `json:"username,omitempty" binding:"required,min=3,max=20"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password,omitempty" binding:"required,min=6"`
 }
